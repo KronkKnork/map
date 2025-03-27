5400c9c433305f92a18a7b9328df995827649b1a
Александр Калачев <alex>
2025-03-27 22:42:03 +0300

    fix

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -9,6 +9,7 @@ import ArrowLeftSvg from './svg/arrow-left.svg';
 import SwapSvg from './svg/swap.svg';
 import OptionsSvg from './svg/options.svg';
 import CloseNavSvg from './svg/close-nav.svg';
+import ArrowLeftWhiteSvg from './svg/arrow-left-white.svg';
 
 const createIconComponent = (SvgComponent) => {
   return ({ width = 24, height = 24, color = '#000', ...props }) => {
@@ -24,3 +25,4 @@ export const ArrowLeft = createIconComponent(ArrowLeftSvg);
 export const Swap = createIconComponent(SwapSvg);
 export const Options = createIconComponent(OptionsSvg);
 export const CloseNav = createIconComponent(CloseNavSvg);
+export const ArrowLeftWhite = createIconComponent(ArrowLeftWhiteSvg);
